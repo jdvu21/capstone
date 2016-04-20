ba3f835fd9c1f9a5e736c0248ef21488e5cc9157
jeff <jeff@debian>
2016-04-20 19:31:01 -0400

    first commit

---
+++ musicexample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+import Tkinter
+
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entry = Tkinter.Entry(self)
+        self.entry.grid(column=0,row=0,sticky='EW')
+
+        button = Tkinter.Button(self,text=u"Click me !")
+        button.grid(column=1,row=0)
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('music player')
+    app.mainloop()
